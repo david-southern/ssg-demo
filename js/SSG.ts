305efd75e9dfd07cc1195dd7cc95c156ec3dd75b
David Southern <david.southern@gmail.com>
2022-10-02 18:55:34 -0600

    Try getting GH pages working

---
+++ js/SSG.ts
@@ -0,0 +1,5 @@
+﻿import { SSGRenderer } from "./ssg.renderer";
+import { Utils } from "./ssg.utils";
+export { Utils }
+
+export const Renderer = new SSGRenderer();
---
+++ js/celestial-object.ts
@@ -0,0 +1,38 @@
+﻿import * as THREE from 'three';
+
+export class CelestialObject {
+    Name?: string;
+    ParentObject?: CelestialObject;
+    ParentName?: string;
+    ChildDepth: number = 0;
+    SystemOrder: number = 0;
+    IsStar: boolean = false;
+    IsSelected: boolean = false;
+    ChildObjects: CelestialObject[] = [];
+    OrbitalSemiMajorAxis: number = 0;
+    OrbitalSemiMinorAxis: number = 0;
+    OrbitalVelocity: number = 0;
+    ObjectMass: number = 0;
+    ObjectRadius: number = 0;
+    OrbitalInclination: number = 0;
+    PhaseAngle: number = 0;
+    ObjectColor: string = null!;
+    OrbitalColor: string = null!;
+
+    Obj3D?: THREE.Object3D;
+
+    constructor(partialObj: Partial<CelestialObject>) {
+        if (partialObj) {
+            Object.assign(this, partialObj);
+            this.ChildObjects = [];
+
+            if (partialObj.ChildObjects && partialObj.ChildObjects.length > 0) {
+                for (const childPartial of partialObj.ChildObjects) {
+                    const childObject = new CelestialObject(childPartial);
+                    childObject.ParentObject = this;
+                    this.ChildObjects.push(childObject);
+                }
+            }
+        }
+    }
+}
